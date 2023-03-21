856f2aba5c1619255c4adb970c57d8b5f92cbdbd
elsiosanchez <45974454+elsiosanchez@users.noreply.github.com>
2023-03-21 12:25:52 -0400

    2 files modified

--- app.vue
+++ app.vue
@@ -1,3 +1,14 @@
 <template>
-  <ActionPanel />
+  <molecula-ActionPanel
+    :showClear="false"
+    :showClose="true"
+    :showCheck="true"
+  >
+    <!-- <template v-slot:slotsLeft>
+      {{ 16515561 }}
+    </template>
+    <template v-slot:slotsRight style="text-align: right">
+      {{ 16515561 }}
+    </template> -->
+  </molecula-ActionPanel>
 </template>
--- components/molecula/ActionPanel.vue
+++ components/molecula/ActionPanel.vue
@@ -1,25 +1,100 @@
 <template>
-  <div class="demo-fit">
-    <el-row class="mb-4">
-      <el-button disabled>Default</el-button>
-      <el-button type="primary" disabled>Primary</el-button>
-      <el-button type="success" disabled>Success</el-button>
-      <el-button type="info" disabled>Info</el-button>
-      <el-button type="warning" disabled>Warning</el-button>
-      <el-button type="danger" disabled>Danger</el-button>
-    </el-row>
-
-    <el-row>
-      <el-button plain disabled>Plain</el-button>
-      <el-button type="primary" plain disabled>Primary</el-button>
-      <el-button type="success" plain disabled>Success</el-button>
-      <el-button type="info" plain disabled>Info</el-button>
-      <el-button type="warning" plain disabled>Warning</el-button>
-      <el-button type="danger" plain disabled>Danger</el-button>
-    </el-row>
+  <div>
+    <el-card>
+      <p :style="{ 'text-align': textAling }">
+        <span :style="{ float: slotsAlingLeft }">
+          <slot name="slotsLeft" />
+        </span>
+        <el-button type="info" @click="actionClear">
+          <template #icon>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="48"
+              viewBox="0 96 960 960"
+              width="48"
+            >
+              <path
+                d="m650 641-42-42 134-105-262-204-102 79-43-43 145-112 360 280-190 147Zm86 86-43-43 97-75 50 37-104 81Zm94 264L641 801 480 926 120 646l50-37 310 241 118-92-43-43-75 59-360-280 120-94L73 234l43-43 757 757-43 43ZM493 484Z"
+              />
+            </svg>
+          </template>
+        </el-button>
+        <el-button type="danger">
+          <template #icon>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="48"
+              viewBox="0 96 960 960"
+              width="48"
+            >
+              <path
+                d="m249 849-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z"
+              />
+            </svg>
+          </template>
+        </el-button>
+        <el-button type="primary">
+          <template #icon>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="48"
+              viewBox="0 96 960 960"
+              width="48"
+            >
+              <path d="M378 810 154 586l43-43 181 181 384-384 43 43-427 427Z" />
+            </svg>
+          </template>
+        </el-button>
+        <span :style="{ float: slotsAlingRight }">
+          <slot name="slotsRight" />
+        </span>
+      </p>
+    </el-card>
   </div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const props = defineProps({
+  showClear: {
+    type: Boolean,
+    required: true,
+  },
+  showClose: {
+    type: Boolean,
+    required: true,
+  },
+  showCheck: {
+    type: Boolean,
+    required: true,
+  },
+  actionClear: {
+    type: Function,
+    default: (pageNumber) => {
+      console.info('implement change page number method', pageNumber);
+    },
+  },
+  disabledClear: {
+    type: Boolean,
+    default: false,
+  },
+  disabledClose: {
+    type: Boolean,
+    default: false,
+  },
+  disabledCheck: {
+    type: Boolean,
+    default: false,
+  },
+  textAling: {
+    type: String,
+    required: false,
+    default: 'Right',
+  },
+  slotsLeft: String,
+  slotsRight: String,
+  slotsAlingLeft: String,
+  slotsAlingRight: String,
+});
+</script>
 
 <style scoped>
 .demo-fit {
